e72fa4d7f622ba0fd5ad2172d9b9f8b83ec3f5ec
Jeff Pittman <geojeff@me.com>
2012-03-08 12:51:05 -0600

    Edited files in the root app directory.

--- apps/todos_three/main.js
+++ apps/todos_three/main.js
@@ -1,29 +1,5 @@
-// ==========================================================================
-// Project:   TodosThree
-// Copyright: @2012 My Company, Inc.
-// ==========================================================================
-/*globals TodosThree */
-
-// This is the function that will start your app running.  The default
-// implementation will load any fixtures you have created then instantiate
-// your controllers and awake the elements on your page.
-//
-// As you develop your application you will probably want to override this.
-// See comments for some pointers on what to do next.
-//
-TodosThree.main = function main() {
-
-  // Step 1: Tell your app it will load via states
-  var statechart = TodosThree.statechart;
-  SC.RootResponder.responder.set('defaultResponder', statechart); 
-  statechart.initStatechart();
-
-  // Step 2. Set the content property on your primary controller.
-  // This will make your app come alive!
-
-  // TODO: Set the content property on your primary controller
-  // ex: MyApp.contactsController.set('content', MyApp.contacts);
-
-} ;
-
+TodosThree.main = function () {
+  TodosThree.statechart.initStatechart();
+};
+ 
 function main() { TodosThree.main(); }
--- apps/todos_three/statechart.js
+++ apps/todos_three/statechart.js
@@ -1,8 +1,17 @@
 TodosThree.statechart = SC.Statechart.create({
 
-  initialState: 'readyState',
-  
-  readyState: SC.State.plugin('TodosThree.ReadyState'),
-  // someOtherState: SC.State.plugin('TodosThree.SomeOtherState')
+  trace: YES,
+ 
+  rootState: SC.State.design({
+    initialSubstate: "READY",
+ 
+    READY: SC.State.plugin('TodosThree.READY'),
+      
+    LOGGING_IN: SC.State.plugin('TodosThree.LOGGING_IN'),
+
+    SHOWING_APP: SC.State.plugin('TodosThree.SHOWING_APP'),
+
+    SHOWING_DESTROY_CONFIRMATION: SC.State.plugin('TodosThree.SHOWING_DESTROY_CONFIRMATION')
+  })
 
 });
