69bd503903de1665cb514b464790be7a1143c5a1
geojeff <geojeff@me.com>
2013-06-19 07:54:06 -0500

    Reworking todo items as github issues.

--- apps/todos/fixtures/todo.js
+++ apps/todos/fixtures/todo.js
@@ -19,70 +19,65 @@ Todos.Todo.FIXTURES = [
 	{
 	guid: 3,
 	isCompleted: NO,
-	title: 'Compatibility -- Check the list of required browsers to support, and ask devs.'
+	title: 'Compatibility -- Check the list of required browsers to support. Does SC offer required browser support?'
 	},
 	{
 	guid: 4,
 	isCompleted: NO,
-	title: 'Structure -- The bower_components directory contains base.css, base.js, bg.png. Do we need to use these?'
+	title: 'Structure -- The bower_components directory contains base.css, base.js, bg.png. Do we need to use this? Especially for CSS -- do we need to adapt as best we can for use in SproutCore todos?'
 	},
 	{
 	guid: 5,
 	isCompleted: NO,
-	title: 'Structure -- In the template, an app.css overrides bower_components/base.css. Worry about?'
+	title: 'Code -- Same question from Structure section about use of base.css and app.css.'
 	},
 	{
 	guid: 6,
 	isCompleted: NO,
-	title: 'Code -- Same question from Structure section about use of base.css and app.css.'
+	title: 'Persistence -- Use SproutCore local storage. Is it important to showcase SproutCore fixtures, with example todos loaded initially? Or, just remove fixtures altogether and initial app load has no todos?'
 	},
 	{
 	guid: 7,
 	isCompleted: NO,
-	title: 'Persistence -- Use SproutCore local storage. Is it important to showcase SproutCore fixtures, with example todos loaded initially? Or, just remove fixtures altogether and initial app load has no todos?'
+	title: 'Routing -- Use SproutCore routing for #/, #/active, #/completed, and do required filtering and displays.'
 	},
 	{
 	guid: 8,
 	isCompleted: NO,
-	title: 'Routing -- Use SproutCore routing for #/, #/active, #/completed, and do required filtering and displays.'
+	title: 'Item -- Add an item edit state to handle editing on double-click.'
 	},
 	{
 	guid: 9,
 	isCompleted: NO,
-	title: 'Item -- Add an item edit state to handle editing on double-click.'
+	title: 'Item -- Add an "X" icon and button that should appear on hovering over a todo item.'
 	},
 	{
 	guid: 10,
 	isCompleted: NO,
-	title: 'Item -- Add an "X" icon and button that should appear on hovering over a todo item.'
+	title: 'Editing -- A new editing state should hide #main, list, and #footer, destroy if empty input, discard on escape. What about blur vs. enter?'
 	},
 	{
 	guid: 11,
 	isCompleted: NO,
-	title: 'Editing -- The new editing state (to be added for a separate todo) should hide other controls, destroy if empty input, discard on escape. Find out about blur vs. enter.'
+	title: 'Counter -- Add a label to the todos counter (e.g., 1 item left, singular, and 2 items left, plural). Assure <strong> tag around Counter number.'
 	},
 	{
 	guid: 12,
-	isCompleted: NO,
-	title: 'Counter -- Add a label to the todos counter (1 item left, singular, and 2 items left, plural). Check on <strong> tag around Counter number.'
+	isCompleted: YES,
+	title: 'Code Style -- Edit files for using tabs as required, instead of spaces.'
 	},
 	{
 	guid: 13,
 	isCompleted: YES,
-	title: 'Code Style -- Edit files for using tabs. Fix use of double spacing vs. tabs.'
+	title: 'New todo -- New todos are entered in the input at the top of the app. Pressing Enter creates the todo, appends it to the todo list and clears the input. Make sure to .trim() the input and then check that it is not empty before creating a new todo.'
 	},
 	{
 	guid: 14,
 	isCompleted: YES,
-	title: 'New todo -- New todos are entered in the input at the top of the app. Pressing Enter creates the todo, appends it to the todo list and clears the input. Make sure to .trim() the input and then check that it is not empty before creating a new todo.'
+	title: 'Mark all as complete -- Checkbox toggles all the todos to the same state as itself. Make sure to clear the checked state after the the "Clear completed" button is clicked. The "Mark all as complete" checkbox should also be updated when single todo items are checked/unchecked. Eg. When all the todos are checked it should also get checked.'
 	},
 	{
 	guid: 15,
-	isCompleted: YES,
-	title: 'Mark all as complete -- Checkbox toggles all the todos to the same state as itself. Make sure to clear the checked state after the the "Clear completed" button is clicked. The "Mark all as complete" checkbox should also be updated when single todo items are checked/unchecked. Eg. Whe	 n all the todos are checked it should also get checked.'
-	},
-	{
-	guid: 16,
 	isCompleted: YES,
 	title: 'Todo Item -- Clicking the checkbox marks the todo as complete by updating it is completed value and toggling the class completed on its parent <li>.'
 	}
