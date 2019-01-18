ef9434a22131896d79c3e4da15ca28d6dea9c95e
Maurits Lamers <mauritslamers@gmail.com>
2019-01-18 11:04:00 +0100

    reversing some changes as the stateObserver cause has been fixed

--- apps/TodosThree/states/ready.js
+++ apps/TodosThree/states/ready.js
@@ -1,21 +1,21 @@
 TodosThree.READY = SC.State.design({
-  enterState: function() {
+  
+  enterState: function () {
     if (SC.instanceOf(TodosThree.store.dataSource, SC.FixturesDataSource)) {
       var allTodos = TodosThree.store.find(SC.Query.local(TodosThree.Todo, { orderBy: 'timestamp DESC' }));
       TodosThree.todosController.set('content', allTodos);
       var completedTodos = TodosThree.store.find(SC.Query.local(TodosThree.Todo, { conditions: 'isCompleted = true' }));
       TodosThree.completedTodosController.set('content', completedTodos);
-      this.gotoState('SHOWING_APP');
     } else {
       this.gotoState('LOGGING_IN');
     }
   },
  
-  // didLoad: function () {
-  //   if (TodosThree.todosController.get('status') === SC.Record.READY_CLEAN) {
-  //     this.gotoState('SHOWING_APP');
-  //   }
-  // }.stateObserves('TodosThree.todosController.status'),
+  didLoad: function () {
+    if (TodosThree.todosController.get('status') === SC.Record.READY_CLEAN) {
+      this.gotoState('SHOWING_APP');
+    }
+  }.stateObserves('TodosThree.todosController.status'),
 
   exitState: function() {
     // Nothing to worry about here.
